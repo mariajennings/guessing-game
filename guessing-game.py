08a35592a202a9fed9f12438ff72c6c2dfb256c5
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-22 15:48:24 -0700

    Finished guessing game!

--- guessing-game.py
+++ guessing-game.py
@@ -8,27 +8,26 @@ number = random.randint(1, 100)
 
 print "shhhh --- the number is %i" % number
 
-name_guess = int(raw_input("What's your guess?"))
+name_guess = None
+
 
 times = 0
 
-if name_guess <= 0 or name_guess > 100:
-    print "Your guess is out of range. Try again!"
-    name_guess = int(raw_input("What's your guess?"))
-    times = times + 1
-
 while name_guess != number:
-    if name_guess < number:
+    try:
+        name_guess = int(raw_input("What's your guess?"))
+    except ValueError:
+        print "That's not an integer!!!!!"
+        times = times + 1
+        continue
+    if name_guess < number and name_guess > 0 and name_guess < 100:
         print "Your guess is too low, try again!"
         times = times + 1
-        name_guess = int(raw_input("What's your guess?"))
-    elif name_guess > number:
+    elif name_guess > number and name_guess > 0 and name_guess < 100:
         print "Your guess it too high, try again!"
         times = times + 1
-        name_guess = int(raw_input("What's your guess?"))
     elif name_guess > 100 or name_guess < 1:
         print "That guess is not in range, try again"
-        name_guess = int(raw_input("What's your guess?"))
 
 if name_guess == number:
     print "You're right! You guess it in %i amount of tries." % times
