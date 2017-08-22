80bf867f550f9c4d019729d6d609ae0b541e5ae1
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-22 15:13:25 -0700

    Built out guessing game

--- guessing-game.py
+++ guessing-game.py
@@ -12,6 +12,11 @@ name_guess = int(raw_input("What's your guess?"))
 
 times = 0
 
+if name_guess <= 0 or name_guess > 100:
+    print "Your guess is out of range. Try again!"
+    name_guess = int(raw_input("What's your guess?"))
+    times = times + 1
+
 while name_guess != number:
     if name_guess < number:
         print "Your guess is too low, try again!"
@@ -21,7 +26,9 @@ while name_guess != number:
         print "Your guess it too high, try again!"
         times = times + 1
         name_guess = int(raw_input("What's your guess?"))
-
+    elif name_guess > 100 or name_guess < 1:
+        print "That guess is not in range, try again"
+        name_guess = int(raw_input("What's your guess?"))
 
 if name_guess == number:
     print "You're right! You guess it in %i amount of tries." % times
