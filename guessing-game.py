126f3d752fe013acc36388405fc392eb387c21a6
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-22 17:52:57 -0700

    version that has max tries working

--- guessing-game.py
+++ guessing-game.py
@@ -6,28 +6,47 @@ print "Ok, %s, I'm thinking of a number between 1 and 100" % name
 
 number = random.randint(1, 100)
 
-print "shhhh --- the number is %i" % number
-
 name_guess = None
-
 
 times = 0
 
-while name_guess != number:
-    try:
-        name_guess = int(raw_input("What's your guess?"))
-    except ValueError:
-        print "That's not an integer!!!!!"
-        times = times + 1
-        continue
-    if name_guess < number and name_guess > 0 and name_guess < 100:
-        print "Your guess is too low, try again!"
-        times = times + 1
-    elif name_guess > number and name_guess > 0 and name_guess < 100:
-        print "Your guess it too high, try again!"
-        times = times + 1
-    elif name_guess > 100 or name_guess < 1:
-        print "That guess is not in range, try again"
+play = "yes"
 
-if name_guess == number:
-    print "You're right! You guess it in %i amount of tries." % times
+best_score = 0
+
+print "shhhh --- the number is %i" % number
+
+while play == "yes":
+        while name_guess != number:
+            if times == 3:
+                print "too many tries!"
+                play = raw_input("would you like to play again? yes or no?")
+            else:
+                try:
+                    name_guess = int(raw_input("What's your guess?"))
+                    times = times + 1
+                except ValueError:
+                    print "That's not an integer!!!!!"
+                    continue
+                if name_guess < number and name_guess > 0 and name_guess < 100:
+                    print "Your guess is too low, try again!"
+                elif name_guess > number and name_guess > 0 and name_guess < 100:
+                    print "Your guess it too high, try again!"
+                elif name_guess > 100 or name_guess < 1:
+                    print "That guess is not in range, try again"
+
+        if name_guess == number:
+            times = times + 1
+            print "You're right! You guessed it in %i amount of tries." % (times)
+            if times < best_score or best_score == 0:
+                best_score = times
+                print "Congratulations! You have the highest score!"
+            elif times > best_score:
+                print "Keep trying to get the best score!"
+            times = 0
+            play = raw_input("would you like to play again? yes or no?")
+            number = random.randint(1, 100)
+            print "shhhh --- the number is %i" % number
+            continue
+
+
